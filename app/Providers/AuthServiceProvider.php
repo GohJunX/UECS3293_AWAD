3782fd0bffd00c8cc257836e6e0affbc1dd4a307
GohJunX <100596563+GohJunX@users.noreply.github.com>
2023-04-05 22:43:08 +0800

    added login signup

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -18,13 +19,16 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
-     * @return void
      */
     public function boot()
     {
-        $this->registerPolicies();
-
+        Gate::define('isAdmin', function($user) {
+            return $user->role == 'admin';
+            });
         //
+        Gate::define('isUser', function($user) {
+            return $user->role == 'user';
+            });
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['adminAuthenticate']], function(){
+    Route::get('/dashboard',function(){
+        return view('home');
+    });
+});
+
+// Route::get('/buy',[HomeController::class,'index2']);
