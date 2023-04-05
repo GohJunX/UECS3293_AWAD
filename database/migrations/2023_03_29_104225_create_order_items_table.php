b5359157baac4ab986584dfb69a9bbfb40a9815c
Ngruen2000 <demondrake00@gmail.com>
2023-04-05 18:58:21 +0800

    Shopping Cart page

--- database/migrations/2023_03_29_104225_create_order_items_table.php
+++ database/migrations/2023_03_29_104225_create_order_items_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->integer('quantity');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
