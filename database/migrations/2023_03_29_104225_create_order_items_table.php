b596a88f028ab874f7a7b9051b880e70f146fc52
williamson922 <100593891+williamson922@users.noreply.github.com>
2023-04-14 11:00:14 +0800

    deleted the columns in database migration

--- database/migrations/2023_03_29_104225_create_order_items_table.php
+++ database/migrations/2023_03_29_104225_create_order_items_table.php
@@ -22,10 +22,7 @@ return new class extends Migration
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->float('price');
             $table->timestamps();
-            $table->unsignedBigInteger('order_id');
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
         });
     }
 
